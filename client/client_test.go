0325f2a9a0c4d7ebfac6638e1fad611cfda305db
petrostrak <pit.trak@gmail.com>
2024-06-04 19:02:03 +0300

    ADD second version for testing client

--- client/client_test.go
+++ client/client_test.go
@@ -8,7 +8,24 @@ import (
 	"time"
 )
 
-func TestNewClient(t *testing.T) {
+func TestNewClientV2(t *testing.T) {
+	c, err := New("localhost:5001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Set(context.Background(), "foo", "1"); err != nil {
+		log.Fatal(err)
+	}
+
+	value, err := c.Get(context.Background(), "foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(value)
+}
+
+func testnewclient(t *testing.T) {
 	c, err := New("localhost:5001")
 	if err != nil {
 		log.Fatal(err)
@@ -16,7 +33,7 @@ func TestNewClient(t *testing.T) {
 	time.Sleep(time.Second)
 
 	for i := 0; i < 10; i++ {
-		fmt.Println("SET =>", fmt.Sprintf("first_name_%d", i))
+		fmt.Println("set =>", fmt.Sprintf("first_name_%d", i))
 		if err := c.Set(context.Background(), fmt.Sprintf("first_name_%d", i), fmt.Sprintf("last_name_%d", i)); err != nil {
 			log.Fatal(err)
 		}
