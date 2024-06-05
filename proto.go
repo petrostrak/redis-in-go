5ca3aaa33ac2e80e978d56ea450fc8c2602988b8
petrostrak <pit.trak@gmail.com>
2024-06-05 12:17:41 +0300

    ADD Hello and client command

--- proto.go
+++ proto.go
@@ -3,61 +3,43 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"log"
 
 	"github.com/tidwall/resp"
 )
 
 const (
-	CommandSET = "SET"
-	CommandGET = "GET"
+	CommandSET    = "set"
+	CommandGET    = "get"
+	CommandHELLO  = "hello"
+	CommandClient = "client"
 )
 
-type Commander interface{}
+type Command interface {
+}
 
 type SetCommand struct {
 	key, val []byte
+}
+
+type ClientCommand struct {
+	value string
+}
+
+type HelloCommand struct {
+	value string
 }
 
 type GetCommand struct {
 	key []byte
 }
 
-func parseCommand(msg string) (Commander, error) {
-	rd := resp.NewReader(bytes.NewBufferString(msg))
-	for {
-		v, _, err := rd.ReadValue()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if v.Type() == resp.Array {
-			for _, val := range v.Array() {
-				switch val.String() {
-				case CommandGET:
-					if len(v.Array()) != 2 {
-						return nil, fmt.Errorf("invalid get command")
-					}
-					cmd := SetCommand{
-						key: v.Array()[1].Bytes(),
-					}
-					return cmd, nil
-				case CommandSET:
-					if len(v.Array()) != 3 {
-						return nil, fmt.Errorf("invalid set command")
-					}
-					cmd := SetCommand{
-						key: v.Array()[1].Bytes(),
-						val: v.Array()[2].Bytes(),
-					}
-					return cmd, nil
-				}
-			}
-		}
+func respWriteMap(m map[string]string) []byte {
+	buf := &bytes.Buffer{}
+	buf.WriteString("%" + fmt.Sprintf("%d\r\n", len(m)))
+	rw := resp.NewWriter(buf)
+	for k, v := range m {
+		rw.WriteString(k)
+		rw.WriteString(":" + v)
 	}
-	return SetCommand{}, fmt.Errorf("invalid or unknown command: %s\n", msg)
+	return buf.Bytes()
 }
